dc4bae71cac50e8f798f49d33136b57ee97fdbb6
8574-dahantc-linux977 <273987074@qq.com>
2024-06-21 09:49:25 +0800

    2024年 06月 21日 星期五 09:49:25 CST

--- k8s_init/yaml/jenkins/prep_jenkins_env.sh
+++ k8s_init/yaml/jenkins/prep_jenkins_env.sh
@@ -1,21 +1,22 @@
 apt-get update
-#jenkins pipeline±Ø±¸¿â
+#jenkins pipelineå¸¸ç”¨å‘½ä»¤
 apt-get install -y jq
-#vim±à¼­Æ÷
+apt-get install -y zip
+#vim
 apt-get install -y vim
-#ÍøÂç²âÊÔ¿â
+#ç½‘ç»œ
 apt-get install -y inetutils-ping
 apt-get install -y telnet
 apt-get install -y dnsutils
-apt-get install -y iproute2 
+apt-get install -y iproute2
 apt-get install -y iptables
-#k8sÏà¹Ø
+
+#k8s
 apt-get install -y apt-transport-https
-curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add - 
+curl https://mirrors.aliyun.com/kubernetes/apt/doc/apt-key.gpg | apt-key add -
 cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
 deb https://mirrors.aliyun.com/kubernetes/apt/ kubernetes-xenial main
 EOF
 apt-get update
 apt-get install -y kubectl
 #apt-get install -y kubelet
-
